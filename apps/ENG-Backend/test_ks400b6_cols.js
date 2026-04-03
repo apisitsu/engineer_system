213e0f5bdb6c0c23899e8b67e32853549ac874eb
apisit_su <apisit.su@minebea.co.th>
2026-04-03 15:17:22 +0700

    clean file .txt un relate2

--- apps/ENG-Backend/test_ks400b6_cols.js
+++
@@ -1,4 +0,0 @@
-const { engPool } = require('./instance/eng_db');
-engPool.query("SELECT column_name FROM information_schema.columns WHERE table_schema='public' AND table_name='tooling_ks400b6' ORDER BY ordinal_position")
-  .then(r => console.log(r.rows.map(x=>x.column_name)))
-  .catch(console.error).finally(()=>process.exit(0));
--- apps/ENG-Backend/test_types.js
+++
@@ -1,5 +0,0 @@
-const { engPool } = require('./instance/eng_db');
-engPool.query(`SELECT table_name, column_name, data_type FROM information_schema.columns WHERE table_name LIKE 'tooling_ks%' AND column_name IN ('dim_a','dim_b')`)
-.then(r => console.table(r.rows))
-.catch(console.error)
-.finally(()=>process.exit(0));
