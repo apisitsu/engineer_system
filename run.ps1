7e7f9d46eea59cb67bda04e59a1aab235cae5566
apisit_su <apisit.su@minebea.co.th>
2026-04-03 15:08:25 +0700

    clean file all file .txt un relate1

--- run.ps1
+++
@@ -1,8 +0,0 @@
-$env:CLAUDE_CODE_GIT_BASH_PATH = "$env:LocalAppData\Programs\Git\bin\bash.exe"
-
-$env:HTTP_PROXY = "http://proxyth.bp.minebea.local:8080"
-$env:HTTPS_PROXY = "http://proxyth.bp.minebea.local:8080"
-
-$env:NODE_TLS_REJECT_UNAUTHORIZED = "0"
-
-claude
