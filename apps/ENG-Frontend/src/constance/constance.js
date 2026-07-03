12cc7c85cf5277906ddb772349d0025005f6d9a5
James <leedzx12@gmail.com>
2026-07-03 12:06:06 +0700

    RELEASE: 1.2.030726

--- apps/ENG-Frontend/src/constance/constance.js
+++ apps/ENG-Frontend/src/constance/constance.js
@@ -10,7 +10,7 @@ export const apiUrl = "http://plbmp130:2005/";
 // // // ----------- DEV -----------
 // export const apiUrl = "http://localhost:2005/";
 // export const apiUrl = "http://plbmp129:2005/";
-export const apiUrl = "http://plbmp118:2005/";
+// export const apiUrl = "http://plbmp118:2005/";
 
 export const server = {
   API_URL: `${apiUrl}`,
